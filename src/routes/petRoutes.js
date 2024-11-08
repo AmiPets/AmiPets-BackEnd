b3ba1cd0d0246810e9ed8e8db180494f3f05d4f9
roddorneles <rodrigodornelesferreira@usp.br>
2024-11-08 16:41:01 -0300

    fix: Remove rota inexistentes de petRoutes

--- src/routes/petRoutes.js
+++ src/routes/petRoutes.js
@@ -10,6 +10,5 @@ router.get('/pets', requireAuth, PetController.getPets);
 router.get('/pets/:id', requireAuth, PetController.getPetById);
 router.put('/pets/:id', requireAuth, PetController.updatePet);
 router.delete('/pets/:id', requireAuth, PetController.deletePet);
-router.post('/pets/search', requireAuth, PetController.searchPets);
 
 export default router;
