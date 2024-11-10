a6ea6b5cc14e62be43adaa2746f34405a0456b93
Hebert Soares <hebertsoaresof@gmail.com>
2024-11-09 21:02:41 -0300

    Impede atualização do campo "isAdmin" por usuários não-administradores

--- src/services/jwtService.js
+++ src/services/jwtService.js
@@ -1,19 +1,30 @@
 import jwt from 'jsonwebtoken';
+import { prismaClient } from '../database/prismaClient.js';
 
-export const generateToken = (userId, email) => {
-  const token = jwt.sign(
-    { userId, email },
-    process.env.JWT_SECRET,
-    { expiresIn: '1h' }
-  );
-  return token;
-};
-
-export const verifyToken = (token) => {
+export const generateToken = async (userId, email) => {
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    return decoded;
+    const user = await prismaClient.adotante.findUnique({
+      where: { id: userId },
+      select: { email: true, isAdmin: true },
+    });
+
+    if (!user) {
+      throw new Error('Usuário não encontrado');
+    }
+
+    console.log('Usuário encontrado:', user);
+
+    const token = jwt.sign(
+      { userId, email: user.email, isAdmin: user.isAdmin },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    console.log('Token gerado:', token);
+
+    return token;
   } catch (error) {
-    return null;
+    console.error('Erro na geração do token:', error);
+    throw error;
   }
 };
