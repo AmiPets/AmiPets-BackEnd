7274457ed50b35315d6d6a3692ca10d10742339a
Hebert Soares <hebertsoaresof@gmail.com>
2024-11-24 15:59:24 -0300

    config cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors'; 
 import petRoutes from './routes/petRoutes.js';
 import adotanteRoutes from './routes/adotanteRoutes.js';
 import adocaoRoutes from './routes/adocaoRoutes.js';
@@ -9,9 +10,17 @@ import setupSwagger from './utils/swaggerConfig.js';
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
+
 setupSwagger(app);
+
 app.use(express.json());
 
+// Rotas da API
 app.use('/api', petRoutes);
 app.use('/api', adotanteRoutes);
 app.use('/api', adocaoRoutes);
@@ -19,5 +28,5 @@ app.use('/api', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
-  console.log(`Servidor rodando na porta ${PORT}`),
+  console.log(`Servidor rodando na porta ${PORT}`)
 );
