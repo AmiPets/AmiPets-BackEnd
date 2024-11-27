d3ef338dee0ed3ad6ae506343ad97a53166ae1e4
roddorneles <rodrigodornelesferreira@usp.br>
2024-11-27 05:02:30 -0300

    feat: Refina rota para filtragem de pets

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import adocaoRoutes from './routes/adocaoRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import setupSwagger from './utils/swaggerConfig.js';
 
+import cors from 'cors';
+
 dotenv.config();
 
 const app = express();
+app.use(cors());
+
 setupSwagger(app);
 app.use(express.json());
 
